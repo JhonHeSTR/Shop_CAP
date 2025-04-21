83355c126e0852b8dd0d54da500395f317caeb60
JhonHeSTR <jhon.arcila@stratesys-ts.com>
2025-04-21 12:05:52 -0500

    The phones are already inserted in the customer

--- srv/Customer/controller/customer.controller.js
+++ srv/Customer/controller/customer.controller.js
@@ -8,28 +8,45 @@ class CustomerController {
     //     console.log('Before, Hi');   
     // }
 
-    on_create(req) {
+    async on_create(req) {
 
-        const { Customers: customers } = this.entities;
+        const { Customers: customers, Phones: phones } = this.entities;
 
         const data = req.data;
 
         try {
 
-            cds.tx(async () => {
+            await cds.tx(async () => {
 
                 const ID = cds.utils.uuid(); // uuid.v4(); 
 
-                let result = await INSERT.into(customers).entries({
+                const exist_document = await SELECT.from(customers).where({ document: data.document });
+
+                if (exist_document.length > 0) {
+                    throw new Error(`Error! The document: ${data.document}, is already in the system.`);
+                }
+
+                await INSERT.into(customers).entries({
                     ID,
                     firstName: data.firstName,
                     lastName: data.lastName,
                     email: data.email,
                     document: data.document,
+                    address: data.address // Composition
                 });
 
-                console.log(result);
-            })
+                if (data.phones && data.phones.length > 0) {
+
+                    const data_phones = data.phones.map(phone => ({
+                        customerId: ID,
+                        customerDocument: data.document,
+                        ...phone
+                    }));
+
+                    await INSERT.into(phones).entries(data_phones);
+                }
+
+            });
 
             return { success: true, message: 'Customer created' };
 
